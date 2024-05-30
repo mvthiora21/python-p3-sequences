6a400632f1af983a0feeab153c3af77d9e62e403
victormuthiora <victormuthiora617@gmail.com>
2024-05-30 10:57:07 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_numbers = []
+    
+    for i in range(length):
+        if i == 0:
+            fibonacci_numbers.append(0)
+        elif i == 1:
+            fibonacci_numbers.append(1)
+        else:
+            fibonacci_numbers.append(fibonacci_numbers[-1] + fibonacci_numbers[-2])
+    
+    print(fibonacci_numbers)
+
+
+    
